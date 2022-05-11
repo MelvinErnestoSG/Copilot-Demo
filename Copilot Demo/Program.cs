1d07dea56c4ebaa02b0ed8e9a5754c44b0f01a42
Melvin Ernesto Santana Garcia <melvinernestosantanagarcia15@gmail.com>
2022-05-11 10:28:45 -0500

    Add project files.

---
+++ Copilot Demo/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+int CalculateDaysBetweenDates(DateTime startDate,DateTime endDate)
+{
+    var totalDays = 0;
+    for (var currentDate = startDate;
+        currentDate <= endDate;
+        currentDate = currentDate.AddDays(1))
+    {
+        totalDays++;
+    }
+    return totalDays;
+}
